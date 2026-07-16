47526e47a0965a61864de8e9700e78b0e916bbb6
dorian.verdel <dorian.verdel@ens-cachan.fr>
2026-07-16 15:40:56 +0200

    mostly finished MVLR analysis

--- set_tex_fonts.py
+++ set_tex_fonts.py
@@ -4,5 +4,6 @@ def set_latex_font(font_size = 10):
     plt.rc('font',  size = font_size)
 
     plt.rc('text', usetex = True)
+    plt.rcParams['text.latex.preamble'] = r'\usepackage{bm}'
 
     plt.rc('font', family='serif')
